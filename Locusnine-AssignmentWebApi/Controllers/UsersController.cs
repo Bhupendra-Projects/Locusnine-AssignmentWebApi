d46b742b841e2b8a267bb0e12d7e583e062cd0c2
Bhupendra-projects <singhwithnaveen@gmail.com>
2020-10-05 18:20:27 +0530

    Locusnine-Backendfiles

---
+++ Locusnine-AssignmentWebApi/Controllers/UsersController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using locusnine.Service.Service;
+using Locusnine.Data;
+using Newtonsoft.Json;
+
+namespace Locusnine_AssignmentWebApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private LocusnineUserDataEntities db = new LocusnineUserDataEntities();
+        int count = 0;
+        // GET: api/Users
+        public string GetUsers()
+        {
+            return JsonConvert.SerializeObject(db.Users.ToList());
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        public string PutUser(User user)
+        {
+            db.Entry(user).State = EntityState.Modified;
+            try
+            {
+                count = db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                throw;
+            }
+            if (count > 0)
+                return JsonConvert.SerializeObject(HttpStatusCode.OK);
+            else
+                return JsonConvert.SerializeObject(HttpStatusCode.InternalServerError);
+        }
+
+        // POST: api/Users
+        [ResponseType(typeof(User))]
+        public string PostUser(User user)
+        {
+            EmailValidation ev = new EmailValidation();
+            string email = ev.AppendEmailSuffix(user.Name);
+            user.Email = email;
+            db.Users.Add(user);
+            try
+            {
+                count = db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            if (count > 0)
+                return JsonConvert.SerializeObject(HttpStatusCode.OK);
+            else
+                return JsonConvert.SerializeObject(HttpStatusCode.InternalServerError);
+        }
+
+        // DELETE: api/Users/5
+        [ResponseType(typeof(User))]
+        public string DeleteUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return JsonConvert.SerializeObject(HttpStatusCode.NotFound);
+            }
+
+            db.Users.Remove(user);
+            count = db.SaveChanges();
+
+            if (count > 0)
+                return JsonConvert.SerializeObject(HttpStatusCode.OK);
+            else
+                return JsonConvert.SerializeObject(HttpStatusCode.InternalServerError);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ locusnine.Service/Service/EmailValidation.cs
@@ -0,0 +1,48 @@
+﻿using Locusnine.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace locusnine.Service.Service
+{
+    public class EmailValidation
+    {
+        private LocusnineUserDataEntities db = new LocusnineUserDataEntities();
+
+        public int CheckEmail(string name)
+        {
+            //get the email address for the name
+            string email = db.Users.Where(x => x.Name == name).OrderByDescending(x => x.Email).Select(x=>x.Email).FirstOrDefault();
+            if (email != null)
+            {
+                string str = email.Split('@')[0];
+                if (str.Length == name.Length)// if its the second record
+                {
+                    return 0;
+                }
+                str = str.Replace(name + "+", String.Empty);//if it has more than 2 records
+                if (str.Length > 0)
+                {
+                    return Convert.ToInt32(str);
+                }
+            }
+            return -1;
+        }
+        /// <summary>
+        /// Get the proper email address
+        /// </summary>
+        /// <param name="name">Name of the User</param>
+        /// <returns></returns>
+        public string  AppendEmailSuffix(string name)
+        {
+            string suffixName = "@locusnine.com";
+            int num=CheckEmail(name);
+            if (num != -1)
+            {
+                return name +"+"+ (++num) + suffixName;
+            }
+            return name+suffixName;
+        }
+    }
+}
